76925137f03339d17ebfed2ea7a4cd4f5feb27e3
Gustavo-Kuze <gustavoksilva3@hotmail.com>
2019-09-22 10:29:37 -0300

    Development project creation

---
+++ tester/src/components/Menu.jsx
@@ -0,0 +1,33 @@
+import '../style/main.css'
+import '../style/menu.css'
+import React from "react";
+
+const Menu = () => {
+    return (
+        <div className="menu-wrap">
+            <input type="checkbox" className="toggler" />
+            <div className="hamburger">
+                <div></div>
+            </div>
+            <div className="menu">
+                <div>
+                    <div>
+                        <ul>
+                            <li>
+                                <a href="#">Home</a>
+                            </li>
+                            <li>
+                                <a href="#">About</a>
+                            </li>
+                            <li>
+                                <a href="#">Services</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Menu;
